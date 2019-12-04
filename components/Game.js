b010a05d82b18499ea66127c2101a12da1d46d82
Dean Radcliffe <dradcliffe@g2.com>
2019-12-04 15:13:39 -0600

    NOJIRA: WIP

---
+++ components/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Game = () => {
+  return (
+    <canvas
+      id="stage"
+      width="480"
+      height="320"
+      style={{ backgroundColor: '#eee' }}
+    ></canvas>
+  );
+};
---
+++ stories/index.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Game } from '../components/Game';
+export default {
+  title: 'Game'
+};
+
+export const canvas = () => <Game />;
